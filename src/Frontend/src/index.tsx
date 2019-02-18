19316870a3cbc60a1390a1ad317ded3a0caf2eed
Peresada Oleg <peresada.oleg@mail.ru>
2019-02-18 23:08:40 +0600

    add frontend

---
+++ src/Frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+
+import { Button } from 'semantic-ui-react';
+
+ReactDOM.render(
+  <Button>Whose button is this?</Button>,
+  document.getElementById('root')
+);
