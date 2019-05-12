522d40eeb7bdeed8359b11a79c5f2426303bbe4b
Pavel <kulbida@hotmail.com>
2019-05-13 02:06:39 +0600

    console tune

--- src/BookmakerConsole/CommandHandler.cs
+++ src/BookmakerConsole/CommandHandler.cs
@@ -32,11 +32,131 @@ namespace BookmakerConsole
         .Split("-", StringSplitOptions.RemoveEmptyEntries)
         .ToList();
 
-      if (textParts.Count != 4)
+      if (textParts.Count == 4)
+      {
+        return UpdateRates(textParts);
+      }
+      else if (textParts.Count == 3)
+      {
+        return DeleteRatesForSpeaker(textParts);
+      }
+      else if (textParts.Count == 2)
+      {
+        return DeleteRatesForBidder(textParts);
+      }
+      else
       {
         return "ERROR - wrong format: [bidder] - [speaker] - [rate] - [ride]";
       }
+    }
 
+    private string DeleteRatesForBidder(List<string> textParts)
+    {
+      var userBidder = textParts[0].Trim();
+      var userRate = Convert.ToUInt32(textParts[1].Trim());
+
+      if (userRate != 0)
+      {
+        return $"ERROR - rate: {userRate} != 0";
+      }
+
+      var bidder = bidders.SingleOrDefault(x => x.Name.ToLower() == userBidder.ToLower());
+
+      if (bidder == null)
+      {
+        return "ERROR - bidder not found";
+      }
+
+      var ridesToUpdate = rides
+        .Where(x => x.Rates.Any(y => y.Bidder.Id == bidder.Id))
+        .Select(x => x.Id)
+        .ToList();
+
+
+      foreach (var rideId in ridesToUpdate)
+      {
+        var ride = client.GetRide(rideId);
+
+        var ratesToDelete = ride.Rates
+          .Where(x => x.Bidder.Id == bidder.Id)
+          .ToList();
+
+        foreach (var rateToDelete in ratesToDelete)
+        {
+          ride.Rates.Remove(rateToDelete);
+        }
+
+        client.UpdateRide(ride);
+      }
+
+
+      bidders = client.GetAllBidders();
+      rides = client.GetAllRides();
+
+      var currentScore = bidders.Single(x => x.Id == bidder.Id).CurrentScore;
+
+      return $"OK - current score: {currentScore}";
+    }
+
+    private string DeleteRatesForSpeaker(List<string> textParts)
+    {
+      var userBidder = textParts[0].Trim();
+      var userSpeaker = textParts[1].Trim();
+      var userRate = Convert.ToUInt32(textParts[2].Trim());
+
+      if (userRate != 0)
+      {
+        return $"ERROR - rate: {userRate} != 0";
+      }
+
+      var bidder = bidders.SingleOrDefault(x => x.Name.ToLower() == userBidder.ToLower());
+
+      if (bidder == null)
+      {
+        return "ERROR - bidder not found";
+      }
+
+      var speaker = speakers.SingleOrDefault(x => x.Name.ToLower() == userSpeaker.ToLower());
+
+      if (speaker == null)
+      {
+        return "ERROR - speaker not found";
+      }
+
+
+      var ridesToUpdate = rides
+        .Where(x => x.Rates.Any(y => y.Bidder.Id == bidder.Id && y.Team == speaker.Id))
+        .Select(x => x.Id)
+        .ToList();
+
+
+      foreach(var rideId in ridesToUpdate)
+      {
+        var ride = client.GetRide(rideId);
+
+        var ratesToDelete = ride.Rates
+          .Where(x => x.Bidder.Id == bidder.Id && x.Team == speaker.Id)
+          .ToList();
+
+        foreach(var rateToDelete in ratesToDelete)
+        {
+          ride.Rates.Remove(rateToDelete);
+        }
+
+        client.UpdateRide(ride);
+      }
+
+
+      bidders = client.GetAllBidders();
+      rides = client.GetAllRides();
+
+      var currentScore = bidders.Single(x => x.Id == bidder.Id).CurrentScore;
+
+      return $"OK - current score: {currentScore}";
+    }
+
+    private string UpdateRates(List<string> textParts)
+    {
       var userBidder = textParts[0].Trim();
       var userSpeaker = textParts[1].Trim();
       var userRate = Convert.ToUInt32(textParts[2].Trim());
@@ -87,7 +207,7 @@ namespace BookmakerConsole
       }
       else
       {
-        var maxId = ride.Rates.Max(x => x.Id);
+        var maxId = ride.Rates.Any() ? ride.Rates.Max(x => x.Id) : 0;
 
         var rateItem = new Rate()
         {
@@ -96,17 +216,19 @@ namespace BookmakerConsole
           RateValue = userRate,
           Team = speaker.Id
         };
+
+        ride.Rates.Add(rateItem);
       }
 
 
       client.UpdateRide(ride);
 
       bidders = client.GetAllBidders();
+      rides = client.GetAllRides();
 
       var currentScore = bidders.Single(x => x.Id == bidder.Id).CurrentScore;
 
       return $"OK - current score: {currentScore}";
     }
-
   }
 }
